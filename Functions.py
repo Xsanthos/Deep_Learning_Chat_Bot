beffdb85778c19b0b4e3117a9881afc69ea176f7
Alexandr Hakobyan <61308427+Xsanthos@users.noreply.github.com>
2020-04-12 04:44:27 +0400

    Addition to functions like NDVI, NDWI, Thermal, etc.

--- Functions.py
+++ Functions.py
@@ -2,25 +2,75 @@ import cv2
 import os
 import imageio
 import numpy as np
+import PIL.Image as Image
 
 
-def imageTake():
+def imageTake(i):
     path = "./Images"
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(i)
+    camera.set(3, 640)
+    camera.set(4, 480)
     return_value, image = camera.read()
-    cv2.imwrite(os.path.join(path, 'sample' + '.jpg'), image)
+    if i < 2:
+        cropped = image[78:332, 140:500]
+        resize = cv2.resize(cropped, (640, 480))
+        cv2.imwrite(os.path.join(path, 'sample' + str(i) + '.png'), resize)
+    else:
+        cv2.imwrite(os.path.join(path, 'sample' + str(i) + '.png'), image)
     del camera
 
 
-def RenameFile(name):
-    os.rename(r'./Images\spectrum.png', r'./Images\\' + name + '.png')
-    os.rename(r'./BMP\spectrum1.png', r'./BMP\\' + name + '.bmp')
+def videoTake():
+    import matplotlib.pyplot as plt
+
+    cam1 = cv2.VideoCapture(1)
+    cam1.set(3, 640)
+    cam1.set(4, 480)
+    cam2 = cv2.VideoCapture(2)
+    while (True):
+        ret, frame1 = cam1.read()
+        cropped = frame1[78:332, 140:500]
+        resize = cv2.resize(cropped, (640, 480))
+        R = resize[:, :, 1]
+        ret2, frame2 = cam2.read()
+        IR = frame2[:, :, 0]
+
+        frame = (IR - R) / (IR + R)
+        ndvi = (frame + 1) / 2
+
+        cv2.imshow('NDVI', ndvi)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cam1.release()
+    cam2.release()
+    cv2.destroyAllWindows()
 
 
-def rgbSplitter():  # Plots separate R G B colored maps
+def renameFile(name):
+    os.rename(r'NDVI\ndvi.png', r'./NDVI\\' + name + '.png')
+
+
+def thermalIR():
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    imageTake(2)
+
+    im = mpimg.imread('./Images/sample2.png')
+    R = im[:, :, 0]
+
+    plt.figure(figsize=(15, 15))
+    plt.title("Thermal")
+    thermal = plt.imshow(R, cmap="inferno")
+    plt.colorbar()
+    plt.show(thermal)
+    plt.imsave('./Thermal/thermal.png', R)
+
+
+def rgbSplitter(i):  # Plots separate R G B colored maps
     import imageio
     import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample.jpg')
+    pic = imageio.imread('./Images/sample' + str(i) + '.png')
 
     plt.figure(figsize=(15, 15))
     fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
@@ -33,9 +83,9 @@ def rgbSplitter():  # Plots separate R G B colored maps
     plt.show(image)
 
 
-def RedChannelSplitter():  # Plots R channel
+def RedChannelSplitter(i):  # Plots R channel
     import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample.jpg')
+    pic = imageio.imread('./Images/sample' + str(i) + '.jpg')
     plt.figure(figsize=(15, 15))
     plt.title('R channel')
     plt.ylabel('Height {}'.format(pic.shape[0]))
@@ -45,9 +95,9 @@ def RedChannelSplitter():  # Plots R channel
     plt.show()
 
 
-def GreenChannelSplitter():  # Plots G channel
+def GreenChannelSplitter(i):  # Plots G channel
     import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample.jpg')
+    pic = imageio.imread('./Images/sample' + str(i) + '.jpg')
     plt.figure(figsize=(15, 15))
     plt.title('G channel')
     plt.ylabel('Height {}'.format(pic.shape[0]))
@@ -57,9 +107,9 @@ def GreenChannelSplitter():  # Plots G channel
     plt.show()
 
 
-def BlueChannelSplitter():  # Plots B channel
+def BlueChannelSplitter(i):  # Plots B channel
     import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample.jpg')
+    pic = imageio.imread('./Images/sample' + str(i) + '.jpg')
     plt.figure(figsize=(15, 15))
     plt.title('B channel')
     plt.ylabel('Height {}'.format(pic.shape[0]))
@@ -77,14 +127,39 @@ def BlueChannelSplitter():  # Plots B channel
                 return int(item["wavelength"])'''
 
 
-def NDVI():  # Calculates and plots NDVI
+def NDVI(i):  # Calculates and plots NDVI
     import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
 
-    im = cv2.imread('./Images/sample.jpg').astype(np.float)
-    B, G, R = cv2.split(im)
+    img1 = mpimg.imread('./Images/sample' + str(i) + '.png')
+    R = img1[:, :, 0]
+    img2 = mpimg.imread('./Images/sample2.png')
+    IR = img2[:, :, 0]
 
-    ndvi = (G - R) / (G + R + 0.0001)
-    ndviplot = plt.imshow(ndvi)
-    plt.show(ndviplot)
-    plt.savefig('./Images/NDVI/ndvi.jpg')
+    plt.figure(figsize=(15, 15))
+    plt.title("NDVI")
+    ndvi = (IR - R) / (IR + R)
+    ndvi_array = (ndvi + 1) / 2
+    ndvi_plot = plt.imshow(ndvi_array, cmap="summer")
 
+    plt.show(ndvi_plot)
+    plt.imsave('./NDVI/ndvi.png', ndvi_array, cmap="summer")
+
+
+def NDWI(i):  # Calculates and plots NDWI
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+
+    img1 = mpimg.imread('./Images/sample' + str(i) + '.png')
+    G = img1[:, :, 1]
+    img2 = mpimg.imread('./Images/sample2.png')
+    IR = img2[:, :, 0]
+
+    plt.figure(figsize=(15, 15))
+    plt.title("NDWI")
+    ndwi = (G - IR) / (G + IR)
+    ndwi_plot = plt.imshow(ndwi, cmap="tab20c")
+    plt.colorbar(orientation="horizontal")
+    plt.show(ndwi_plot)
+    plt.savefig('./NDWI/ndwi.png')
+    plt.imsave('./NDWI/ndwi.png', ndwi, cmap="gray")
--- Main.py
+++ Main.py
@@ -1,10 +1,41 @@
+import time
 from Functions import *
-import os
+import matplotlib.pyplot as plt
 
-choice = input("Would you like to scan the sample? y/n")
-if choice == "y":
-    imageTake()
-    changeN = input("Please rename file")
-    os.rename(r'./Images\spectrum.png', r'./Images\\'+changeN+'.png')
-    os.rename(r'./BMP\spectrum1.png', r'./BMP\\' + changeN + '.bmp')
 
+def VideoAnalyser():
+    videoTake()
+
+
+def ImageAnalyser():
+    print("############## Spectral Analyser ##############\n Welcome!\n")
+    time.sleep(0.1)
+    print("Would you like to scan a sample for analysis?")
+    time.sleep(1)
+    choice = input("Yes/No ------ ")
+    if choice == "Yes":
+        t = "Yes"
+        while t == "Yes":
+            imageTake(1)
+            imageTake(2)
+            NDVI(1)
+            time.sleep(1)
+            file_name = input("Please rename sample file ---")
+            renameFile(file_name)
+            plt.close()
+            time.sleep(1)
+            t = input("Would you like to scan another sample? (Yes/No) ---- ")
+            if t == "No":
+                choice = "No"
+
+    if choice == "No":
+        plt.close()
+        time.sleep(1)
+        print("Thank you for using the app!")
+
+
+def main():
+    thermalIR()
+
+
+main()
