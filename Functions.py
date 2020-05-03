234f61f684096f824ba3979d3a4e2a9724291228
Alexandr Hakobyan <61308427+Xsanthos@users.noreply.github.com>
2020-05-04 00:36:17 +0400

    Added Main python code and two features of the project.

--- Functions.py
+++ Functions.py
@@ -1,79 +1,34 @@
-import cv2
 import os
-import imageio
 import numpy as np
-import PIL.Image as Image
+import time
 
 
-def imageTake(i):
-    path = "./Images"
-    camera = cv2.VideoCapture(i)
-    camera.set(3, 640)
-    camera.set(4, 480)
-    return_value, image = camera.read()
-    if i < 2:
-        cropped = image[78:332, 140:500]
-        resize = cv2.resize(cropped, (640, 480))
-        cv2.imwrite(os.path.join(path, 'sample' + str(i) + '.png'), resize)
-    else:
-        cv2.imwrite(os.path.join(path, 'sample' + str(i) + '.png'), image)
-    del camera
-
-
-def videoTake():
-    import matplotlib.pyplot as plt
-
-    cam1 = cv2.VideoCapture(1)
-    cam1.set(3, 640)
-    cam1.set(4, 480)
-    cam2 = cv2.VideoCapture(2)
-    while (True):
-        ret, frame1 = cam1.read()
-        cropped = frame1[78:332, 140:500]
-        resize = cv2.resize(cropped, (640, 480))
-        R = resize[:, :, 1]
-        ret2, frame2 = cam2.read()
-        IR = frame2[:, :, 0]
-
-        frame = (IR - R) / (IR + R)
-        ndvi = (frame + 1) / 2
-
-        cv2.imshow('NDVI', ndvi)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cam1.release()
-    cam2.release()
-    cv2.destroyAllWindows()
-
-
-def renameFile(name):
-    os.rename(r'NDVI\ndvi.png', r'./NDVI\\' + name + '.png')
+def renameFile(name, location):
+    os.rename(r'./NDVI\ndvi.png', r'./' + location + '\\' + name + '.png')
 
 
 def thermalIR():
     import matplotlib.pyplot as plt
     import matplotlib.image as mpimg
-    imageTake(2)
 
-    im = mpimg.imread('./Images/sample2.png')
-    R = im[:, :, 0]
+    im = mpimg.imread('./Images/NIR.png')
+    IR = im[:, :, 0]
 
     plt.figure(figsize=(15, 15))
     plt.title("Thermal")
-    thermal = plt.imshow(R, cmap="inferno")
+    thermal = plt.imshow(IR, cmap="inferno")
     plt.colorbar()
     plt.show(thermal)
-    plt.imsave('./Thermal/thermal.png', R)
+    plt.imsave('./Thermal/thermal.png', IR)
 
 
 def rgbSplitter(i):  # Plots separate R G B colored maps
     import imageio
     import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample' + str(i) + '.png')
+    pic = imageio.imread('./Images/' + str(i) + '.jpg')
 
     plt.figure(figsize=(15, 15))
-    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+    fig, ax = plt.subplots(nrows=1, ncols=7, figsize=(15, 5))
 
     for c, ax in zip(range(3), ax):
         split_img = np.zeros(pic.shape, dtype="uint8")
@@ -81,42 +36,6 @@ def rgbSplitter(i):  # Plots separate R G B colored maps
         image = ax.imshow(split_img)
 
     plt.show(image)
-
-
-def RedChannelSplitter(i):  # Plots R channel
-    import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample' + str(i) + '.jpg')
-    plt.figure(figsize=(15, 15))
-    plt.title('R channel')
-    plt.ylabel('Height {}'.format(pic.shape[0]))
-    plt.xlabel('Width {}'.format(pic.shape[1]))
-
-    plt.imshow(pic[:, :, 0])
-    plt.show()
-
-
-def GreenChannelSplitter(i):  # Plots G channel
-    import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample' + str(i) + '.jpg')
-    plt.figure(figsize=(15, 15))
-    plt.title('G channel')
-    plt.ylabel('Height {}'.format(pic.shape[0]))
-    plt.xlabel('Width {}'.format(pic.shape[1]))
-
-    plt.imshow(pic[:, :, 1])
-    plt.show()
-
-
-def BlueChannelSplitter(i):  # Plots B channel
-    import matplotlib.pyplot as plt
-    pic = imageio.imread('./Images/sample' + str(i) + '.jpg')
-    plt.figure(figsize=(15, 15))
-    plt.title('B channel')
-    plt.ylabel('Height {}'.format(pic.shape[0]))
-    plt.xlabel('Width {}'.format(pic.shape[1]))
-
-    plt.imshow(pic[:, :, 0])
-    plt.show()
 
 
 '''def RGBtoWavelength(r, g, b):  ## May not be used!
@@ -127,39 +46,37 @@ def BlueChannelSplitter(i):  # Plots B channel
                 return int(item["wavelength"])'''
 
 
-def NDVI(i):  # Calculates and plots NDVI
+def NDVI():  # Calculates and plots NDVI
     import matplotlib.pyplot as plt
     import matplotlib.image as mpimg
 
-    img1 = mpimg.imread('./Images/sample' + str(i) + '.png')
-    R = img1[:, :, 0]
-    img2 = mpimg.imread('./Images/sample2.png')
-    IR = img2[:, :, 0]
+    img1 = mpimg.imread('./Images/RGB.png')
+    img2 = mpimg.imread('./Images/NIR.png')
+    R = img1[:, :, 2]
+    IR = img2[:, :, 2]
 
     plt.figure(figsize=(15, 15))
     plt.title("NDVI")
-    ndvi = (IR - R) / (IR + R)
+    ndvi = (IR - R) / (IR + R + 0.0001)
     ndvi_array = (ndvi + 1) / 2
     ndvi_plot = plt.imshow(ndvi_array, cmap="summer")
-
     plt.show(ndvi_plot)
     plt.imsave('./NDVI/ndvi.png', ndvi_array, cmap="summer")
 
 
-def NDWI(i):  # Calculates and plots NDWI
+# Calculates and plots NDWI
+def NDWI():
     import matplotlib.pyplot as plt
     import matplotlib.image as mpimg
 
-    img1 = mpimg.imread('./Images/sample' + str(i) + '.png')
+    img1 = mpimg.imread('./Images/RGB.png')
+    img2 = mpimg.imread('./Images/NIR.png')
     G = img1[:, :, 1]
-    img2 = mpimg.imread('./Images/sample2.png')
-    IR = img2[:, :, 0]
+    IR = img2[:, :, 2]
 
     plt.figure(figsize=(15, 15))
     plt.title("NDWI")
-    ndwi = (G - IR) / (G + IR)
+    ndwi = (G - IR) / (G + IR + 0.0001)
     ndwi_plot = plt.imshow(ndwi, cmap="tab20c")
-    plt.colorbar(orientation="horizontal")
     plt.show(ndwi_plot)
-    plt.savefig('./NDWI/ndwi.png')
-    plt.imsave('./NDWI/ndwi.png', ndwi, cmap="gray")
+    plt.imsave('./NDWI/ndwi.png', ndwi, cmap="tab20c")
--- Main.py
+++ Main.py
@@ -1,41 +1,100 @@
-import time
 from Functions import *
-import matplotlib.pyplot as plt
+from tkinter.ttk import *
+from tkinter import *
+import tkinter as tk
 
 
-def VideoAnalyser():
-    videoTake()
+def gui():
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.title("Spectral Analyser")
+    root.configure(bg="#84817a")
+    root.geometry("900x640")
 
+    # Creating Frame
+    frame = tk.Frame(root, bg="#aaa69d")
+    frame.place(relwidth=0.95, relheight=0.95, relx=0.025, rely=0.025)
 
-def ImageAnalyser():
-    print("############## Spectral Analyser ##############\n Welcome!\n")
-    time.sleep(0.1)
-    print("Would you like to scan a sample for analysis?")
-    time.sleep(1)
-    choice = input("Yes/No ------ ")
-    if choice == "Yes":
-        t = "Yes"
-        while t == "Yes":
-            imageTake(1)
-            imageTake(2)
-            NDVI(1)
-            time.sleep(1)
-            file_name = input("Please rename sample file ---")
-            renameFile(file_name)
-            plt.close()
-            time.sleep(1)
-            t = input("Would you like to scan another sample? (Yes/No) ---- ")
-            if t == "No":
-                choice = "No"
+    # Embedding Terminal
+    TextField = tk.Text(frame, bg="#222222", fg="#9e1a1a", width=82, height=20)
+    TextField.grid(row=5, column=1)
 
-    if choice == "No":
-        plt.close()
-        time.sleep(1)
-        print("Thank you for using the app!")
+    # Creating Labels
+    label1 = tk.Label(frame, text="Vegitation Index \nAnalyser", font=('calibri', 10, 'italic'),
+                      bg=("#aaa69d"))
+    label1.grid(row=2, column=0)
+
+    label2 = tk.Label(frame, text="Water Index \nAnalyser", font=('calibri', 10, 'italic'), bg=("#aaa69d"))
+    label2.grid(row=2, column=2)
+
+    label3 = tk.Label(frame, text="Infrared \nAnalyser", font=('calibri', 10, 'italic'), bg=("#aaa69d"))
+    label3.grid(row=4, column=0)
+
+    Title = tk.Label(frame, text="Spectral Analyser", font=('calibri', 36, 'bold'), bg=("#aaa69d"))
+    Title.grid(row=1, column=1, sticky=N + E + W + S)
+
+    SubTitle = tk.Label(frame, text="  This application allows \nusers to calculate NDVI,\n NDWI and IR values and\n "
+                                    "save them as images  ", font=('calibri', 14), bg=("#aaa69d"))
+    SubTitle.grid(row=3, column=1, sticky=N + E + W + S)
+
+    # GUI functions
+    def onClickNDVI():
+        TextField.delete(1.0, tk.END)
+        prompt = "Please rename your file:--- \n"
+        TextField.insert(tk.END, prompt)
+        NDVI()
+        time.sleep(5)
+        TextField.delete(1.0, tk.END)
+        Name = TextField.get(1.0, tk.END)
+        renameFile(Name)
+
+    def onClickThermal():
+        TextField.delete(1.0, tk.END)
+        prompt = "Please rename your file:--- \n"
+        TextField.insert(tk.END, prompt)
+        thermalIR()
+        time.sleep(5)
+        TextField.delete(1.0, tk.END)
+        Name = TextField.get(1.0, tk.END)
+        renameFile(Name)
+        renameFile(Name)
+
+    def onClickNDWI():
+        TextField.delete(1.0, tk.END)
+        prompt = "Please rename your file:--- \n"
+        TextField.insert(tk.END, prompt)
+        NDWI()
+        time.sleep(5)
+        TextField.delete(1.0, tk.END)
+        Name = TextField.get(1.0, tk.END)
+        renameFile(Name)
+        renameFile(Name)
+
+    def Quit():
+        root.destroy()
+
+    # Creating Button
+    btn1 = Button(frame, text="NDVI", font=('calibri', 20, 'bold'), fg="#9e1a1a", bg="#84817a", state="normal",
+                  command=onClickNDVI, borderwidth='2')
+    btn1.grid(row=1, column=0)
+
+    btn2 = Button(frame, text="NDWI", font=('calibri', 20, 'bold'), fg="#9e1a1a", bg="#84817a", state="normal",
+                  command=onClickNDWI, borderwidth='2')
+    btn2.grid(row=1, column=2)
+
+    btn3 = Button(frame, text="   IR   ", font=('calibri', 20, 'bold'), fg="#9e1a1a", bg="#84817a", state="normal",
+                  command=onClickThermal, borderwidth='2')
+    btn3.grid(row=3, column=0)
+
+    btn4 = Button(frame, text="  Quit  ", font=('calibri', 20, 'bold'), fg="#9e1a1a", bg="#84817a", state="normal",
+                  command=Quit, borderwidth='2')
+    btn4.grid(row=3, column=2)
+
+    root.mainloop()
 
 
 def main():
-    thermalIR()
+    gui()
 
 
 main()
